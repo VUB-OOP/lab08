82c97c16c622df77d1306b6a7fff208f2e45d3b8
Krunoslav Husak <khusak@vub.hr>
2024-11-23 11:57:01 +0000

    Konstruktor koji će postaviti stanje računa na odabranu vrijednost

--- isplatni_racun.cpp
+++ isplatni_racun.cpp
@@ -1,1 +1,4 @@
 #include "isplatni_racun.h"
+
+// prosljeđivanje argumenata baznom konstruktoru
+IsplatniRacun::IsplatniRacun(double stanje, std::string valuta) : Racun(stanje, valuta) {}
--- isplatni_racun.h
+++ isplatni_racun.h
@@ -1,4 +1,6 @@
 #include "racun.h"
 
 class IsplatniRacun : public Racun {
+public:
+  IsplatniRacun(double stanje, std::string valuta = "EUR");
 };
