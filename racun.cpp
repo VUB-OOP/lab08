eca9162dc94f9befd8268182d53b3d843b4489c9
Krunoslav Husak <khusak@vub.hr>
2024-11-23 11:43:23 +0000

    Metode dohvatiStanje koja vraća brojčanu vrijednost i dohvatiStanjeTekst koja vraća tekstualno stanje računa

--- racun.cpp
+++ racun.cpp
@@ -1,1 +1,9 @@
 #include "racun.h"
+
+double Racun::dohvatiStanje() {
+  return stanje;
+}
+
+std::string Racun::dohvatiStanjeTekst() {
+  return std::to_string(stanje) + " " + valuta;
+}
--- racun.h
+++ racun.h
@@ -1,6 +1,10 @@
 #include <string>
 
 class Racun {
+public:
+  double dohvatiStanje();
+  std::string dohvatiStanjeTekst();
+
 protected:
   double stanje;
   std::string valuta;
